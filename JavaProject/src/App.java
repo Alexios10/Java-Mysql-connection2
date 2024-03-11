4886035eeb9e2bf0bdcb9d0f0f38da4eac0681bf
Alexios10 <sattar_saud@hotmail.com>
2024-03-11 23:08:16 +0100

    Java Mysql connection

--- JavaProject/src/App.java
+++ JavaProject/src/App.java
@@ -1,16 +1,41 @@
-
+import java.util.Scanner;
 
 public class App {
-    public static void checkAge(Student student) {
-        if (student.getAge() < 18) {
-            System.out.println("You are not allowed.");
-        } else {
-            System.out.println("You are allowed.");
-        }
-    }
+
+    static int x = 1;
+    static int y = 2;
+
+    static int temp;
+
 
     public static void main(String[] args) {
-        Student student = new Student("sattar", 15);
-        checkAge(student);
+
+        temp = x;
+        x = y;
+        y = temp;
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        try {
+//            System.out.println("Whats ur name ");
+//            String name = scanner.nextLine();
+//
+//            System.out.println("How old are you");
+//            int age = scanner.nextInt();
+//            scanner.nextLine();
+//            System.out.println("Whats ur hobby");
+//            String hobby = scanner.nextLine();
+//
+//            System.out.println("Hello " + name);
+//            System.out.println("You are " + age + " years old");
+//            System.out.println("You hobby is " + hobby);
+//        } catch (Exception e) {
+//
+//            System.out.println("Somthing went wrong: ");
+//            System.err.println(e);
+//        }
+
     }
 }
